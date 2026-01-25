259508e9d7d08d83cbb8f0e2909e78d9cf0ee41d
oksana-chornobai <chernobay.oks@icloud.com>
2026-01-25 14:56:02 +0200

    Clear index.js in main branch

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
-require('dotenv').config();
-const express = require('express');
-
-const app = express();
-
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// main branch: базовий index.js
