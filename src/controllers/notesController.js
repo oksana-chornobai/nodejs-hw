bca003440092a18c70609373d3f801592170b5cf
oksana-chornobai <chernobay.oks@icloud.com>
2026-02-08 16:51:21 +0200

    updated notesController.js

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -66,7 +66,6 @@ export const updateNote = async (req, res, next) => {
 
   const note = await Note.findOneAndUpdate({ _id: noteId, userId: req.user._id }, req.body, {
     new: true,
-    includeResultMetadata: true,
   });
 
   if (!note) {
