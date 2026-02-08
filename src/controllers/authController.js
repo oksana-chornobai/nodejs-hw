2e47981c038dc46d6fd1e618e77ba97fab88298f
oksana-chornobai <chernobay.oks@icloud.com>
2026-02-08 19:05:15 +0200

    fixed authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,7 +7,7 @@ import handlebars from 'handlebars';
 import { User } from '../models/user.js';
 import { Session } from '../models/session.js';
 import { createSession, setSessionCookies } from '../services/auth.js';
-import { sendMail } from '../utils/sendMail.js';
+import { sendEmail } from '../utils/sendMail.js';
 
 export const registerUser = async (req, res, next) => {
   const { email, password } = req.body;
@@ -124,7 +124,7 @@ export const requestResetEmail = async (req, res, next) => {
   });
 
   try {
-    await sendMail({
+    await sendEmail({
       from: process.env.SMTP_FROM,
       to: email,
       subject: 'Reset your password',
